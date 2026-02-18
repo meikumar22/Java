a3a1ba5f1ee0208608169a78c15ae48058d249b7
Meikumar <meikumar49@gmail.com>
2026-02-18 18:35:48 +0530

    Add files via upload

---
+++ Basic/Problem17.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem17 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int dh = scan.nextInt();
+        int hr = scan.nextInt();
+        int dc = scan.nextInt();
+        int tc = scan.nextInt();
+        int ap = scan.nextInt();
+
+        int projectcost = ((dh*hr)+dc+tc-ap);
+        System.out.print(projectcost);
+    }
+    
+}
