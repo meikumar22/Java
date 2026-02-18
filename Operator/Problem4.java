86e624e0eeca09d2f3049d7f32644b96462352b1
Meikumar <meikumar49@gmail.com>
2026-02-19 05:17:34 +0530

    Add files via upload

---
+++ Operator/Problem4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Problem4 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int creditlimit = scan.nextInt();
+        int purchase_amount = scan.nextInt();
+        System.out.print("Enter 0 or 1 : ");
+        int cardblocked = scan.nextInt();
+
+        if(purchase_amount<=creditlimit){
+            if(cardblocked==0){
+                System.out.println("Approved");
+            }
+            else if(cardblocked==1){
+                System.out.println("Declined");
+            }
+            else{
+                System.out.println("Enter valid card number");
+            }
+        }
+        else{
+            System.out.println("Declined");
+        }
+        
+    }
+}
