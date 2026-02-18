bf0f737a20b54881a162e286c91aabc34ca890ac
Meikumar <meikumar49@gmail.com>
2026-02-18 18:34:42 +0530

    Add files via upload

---
+++ Basic/Problem13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem13 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+
+        System.out.println((a*b)+c-d);
+}
+}
