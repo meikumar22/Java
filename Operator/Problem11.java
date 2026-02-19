719c1e2bde9f773a91461c0484341c2c3ae071d0
Meikumar <meikumar49@gmail.com>
2026-02-19 10:12:01 +0530

    Add files via upload

---
+++ Operator/Problem11.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Problem11 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int basebox = scan.nextInt();
+        int level = scan.nextInt();
+
+        int total_capacity = basebox<<level;
+
+        System.out.print(total_capacity);
+    }
+}
