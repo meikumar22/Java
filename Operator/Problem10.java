33782bd553124e7da59cafa1af56b90b31218684
Meikumar <meikumar49@gmail.com>
2026-02-19 07:06:23 +0530

    Add files via upload

---
+++ Operator/Problem10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Problem10 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int units = scan.nextInt();
+        System.out.print("Enter voltagefluctuation 0 or 1 : ");
+        int voltage = scan.nextInt();
+
+        if(500<units || voltage==1){
+            System.out.print("Alert");
+        }
+        else{
+            System.out.print("Normal");
+        }
+    }
+}
