3cb3d36c21d351f3b9b6ca85330b07736b538ccf
Meikumar <meikumar49@gmail.com>
2026-03-14 23:07:46 +0530

    Add files via upload

---
+++ Nested loop/Demo8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char ch = scan.next().charAt(0);
+
+        System.out.println((int) ch);
+        
+    }
+}
