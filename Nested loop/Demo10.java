5d94fd901400668f34994d9df65946106681ebaf
Meikumar <meikumar49@gmail.com>
2026-03-14 23:08:20 +0530

    Add files via upload

---
+++ Nested loop/Demo10.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Demo10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int k=0; k<n-i; k++){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<=i; j++){
+                System.out.print((char) ('A' + j) + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
