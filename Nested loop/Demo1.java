20ded609cedc8181f362013633d3b7ab0f9b16fb
Meikumar <meikumar49@gmail.com>
2026-03-16 21:57:23 -0700

    Update Demo1.java

--- Nested loop/Demo1.java
+++ Nested loop/Demo1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Demo1 {
+public class Demo1 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
