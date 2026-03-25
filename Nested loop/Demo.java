d636a4248ea91aebb75dff764633aa4ee304d8c0
Meikumar <meikumar49@gmail.com>
2026-03-25 20:56:17 +0530

    Add files via upload

---
+++ Nested loop/Demo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
