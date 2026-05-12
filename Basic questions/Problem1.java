8ce7eaec7c029aab57442d74e735c12c4dbc2bf8
Meikumar <meikumar49@gmail.com>
2026-05-12 06:32:04 -0700

    Add files via upload

---
+++ Basic questions/Problem1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        int sum = 0;
+
+        sum = a + b;
+
+        System.out.println(sum);
+    }
+}
