626386628ac3d9453e778a98247c138b1daaa96a
Meikumar <meikumar49@gmail.com>
2026-05-12 07:38:03 -0700

    Add files via upload

---
+++ Basic questions/Problem8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int sum = 1;
+
+        for(int i=1; i<=n; i++){
+            sum = sum * i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
