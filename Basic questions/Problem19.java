ec16ef84229576bd8650278b79ecbe1ee74b1898
Meikumar <meikumar49@gmail.com>
2026-05-14 01:30:23 -0700

    Add files via upload

--- Basic questions/Problem19.java
+++ Basic questions/Problem19.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class Problem19 {
-    
+    public static void main(String[] args) {
+        Scanner scan =  new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int rev = 0;
+
+        for(; n!=0; n=n/10){
+            rev = rev * 10 + n%10;
+        }
+        System.out.println(rev);
+    }
 }
