ff0c99f4eed30f6eb7137aebef4edcc9a28430b5
Meikumar <meikumar49@gmail.com>
2026-05-14 01:19:54 -0700

    Add files via upload

---
+++ Basic questions/Problem15.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Problem15 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=n; i>=1; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
