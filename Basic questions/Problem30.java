ece4894d2885dd773201eb3800b69c3a1dab0517
Meikumar <meikumar49@gmail.com>
2026-05-15 21:58:27 -0700

    Add files via upload

---
+++ Basic questions/Problem30.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Problem30 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=1; i<=n; i++){
+            if(i==6){
+                break;
+            }
+            System.out.println(i + " ");
+        }
+        System.out.println("Hello");
+    }
+}
