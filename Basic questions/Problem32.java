91767e1b5447c82c09563aef905f500499703fea
Meikumar <meikumar49@gmail.com>
2026-05-17 21:59:22 -0700

    Add files via upload

---
+++ Basic questions/Problem32.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Problem32 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int day = scan.nextInt();
+
+        switch(day){
+            case 1:
+                {
+                    System.out.println("Monday");
+                    break;
+                }
+            case 2:
+                {
+                    System.out.println("Tuesday");
+                    break;
+                }
+            case 3:
+                {
+                    System.out.println("Wednesday");
+                    break;
+                }
+            case 4:
+                {
+                    System.out.println("Thursday");
+                    break;
+                }
+            case 5:
+                {
+                    System.out.println("Friday");
+                    break;
+                }
+            case 6:
+                {
+                    System.out.println("Weekend");
+                    break;
+                }
+            default:
+                {
+                    System.out.println("Invalid day");
+                }
+        }
+    }
+}
